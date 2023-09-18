193f909d96965b86fb410a21fd4662146c4e1b70
SnowGRH <gergelyovinczeff@gmail.com>
2023-09-18 15:42:52 +0200

    OOP kész

--- src/kartyatrukkkonzolon/Lap.java
+++ src/kartyatrukkkonzolon/Lap.java
@@ -4,16 +4,17 @@ import java.util.Scanner;
 
 public class Lap {
 
-    private static final Scanner sc = new Scanner(System.in);
+    private String[] pakli = new String[22];
+    private String leiras;
+    String[] szinek = {"P", "T", "Z", "M"};
+    String[] ertekek = {"Ász", "Kir", "Fel", "X", "IX", "VIII"};
 
-    private static int melyik() {
-        boolean jo;
-        int oszlop;
-        do {
-            System.out.print("melyik oszlop (1-3): ");
-            oszlop = sc.nextInt();
-            jo = oszlop >= 1 && oszlop <= 3;
-        } while (!jo);
-        return oszlop;
+    public Lap(String leiras) {
+        this.leiras = leiras;
     }
+
+    public String getLeiras() {
+        return leiras;
+    }
+
 }

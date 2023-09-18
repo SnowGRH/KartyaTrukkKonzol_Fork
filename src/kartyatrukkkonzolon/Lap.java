3296fe40f94307554fd394c7762e954db914a19e
SnowGRH <gergelyovinczeff@gmail.com>
2023-09-18 15:22:10 +0200

    első

---
+++ src/kartyatrukkkonzolon/Lap.java
@@ -0,0 +1,19 @@
+package kartyatrukkkonzolon;
+
+import java.util.Scanner;
+
+public class Lap {
+
+    private static final Scanner sc = new Scanner(System.in);
+
+    private static int melyik() {
+        boolean jo;
+        int oszlop;
+        do {
+            System.out.print("melyik oszlop (1-3): ");
+            oszlop = sc.nextInt();
+            jo = oszlop >= 1 && oszlop <= 3;
+        } while (!jo);
+        return oszlop;
+    }
+}
